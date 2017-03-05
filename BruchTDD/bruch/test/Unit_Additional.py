7e478694fa2049728730d0ccd908355f67318fa1
Moritz Mühlehner <mmuehlehner@student.tgm.ac.at>
2017-03-05 15:57:30 +0100

    fexed additional tests

--- BruchTDD/bruch/test/Unit_Additional.py
+++ BruchTDD/bruch/test/Unit_Additional.py
@@ -1,7 +1,7 @@
 import unittest
 from bruch.Bruch import *
 
-class AdditionalTests(unittest.TestCase):
+class TestAdditionalTests(unittest.TestCase):
 
     def setUp(self):
         self.b = Bruch(9, 5)
@@ -13,21 +13,21 @@ class AdditionalTests(unittest.TestCase):
         del self.b, self.b2, self.b3
         pass
 
-    def addtest1(self):
-        self.assertRaises(ZeroDivisionError, self.b2.__truediv__, Bruch(1, 0))
+    def testaddtest1(self):
+        self.assertRaises(ZeroDivisionError, self.b2.__truediv__, 0)
 
-    def addtest2(self):
-        self.b3 = self.b+1
-        assert(self.b3 == Bruch(19, 5))
+    def testaddtest2(self):
+        self.b3 = self.b + 1
+        assert(self.b3 == Bruch(14, 5))
 
-    def addtest3(self):
+    def testaddtest3(self):
         self.b3 = self.b3 - self.b2
         assert(self.b3 == Bruch(-11, 10))
 
-    def addtest4(self):
+    def testaddtest4(self):
         self.b2 = Bruch(6, 10) * 5
         assert(self.b2 == 3)
 
-    def addtest5(self):
+    def testaddtest5(self):
         self.b2 = self.b / self.b3
         assert(self.b2 == Bruch(18, 7))
